c4d1184fea8ad92ac24331b40098ab68832aa4ad
automationmktg005-svg <automationmktg005@gmail.com>
2025-09-19 17:43:22 +0500

    Update main.py

--- main.py
+++ main.py
@@ -475,4 +475,6 @@ def health_check():
 
 # --- RUN THE APP ---
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=False)
+
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port)
